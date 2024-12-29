f3433cc83cf98996a0f19e12b3137cb9249257cd
vinayak banoth <code.editor>
2024-12-29 19:56:20 +0530

    Updated for automatic dropdown when cursor placed on menu item

--- hosp-proj/src/components/Navbar.js
+++ hosp-proj/src/components/Navbar.js
@@ -1,36 +1,69 @@
-import React from 'react';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'; 
+import React, { useState } from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyNavbar() {
+   
+    const [showDropdown, setShowDropdown] = useState('');
+
+  
+    const handleMouseEnter = (dropdown) => {
+        setShowDropdown(dropdown);
+    };
+
+    const handleMouseLeave = () => {
+        setShowDropdown('');
+    };
+
     return (
         <header>
             <Navbar bg="light" variant="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#">LIVE</Navbar.Brand> {/* Brand logo link */}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* Toggle button for mobile view */}
+                    <Navbar.Brand href="#">LIVE</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            {/* Basic Nav Links */}
                             <Nav.Link href="#home">About Us</Nav.Link>
-                            <Nav.Link href="#features">Our doctors</Nav.Link>
-                            
-                            {/* Dropdown Menu */}
-                            <NavDropdown title="Speciality" id="basic-nav-dropdown">
+                            <Nav.Link href="#features">Our Doctors</Nav.Link>
+
+                           
+                            <NavDropdown
+                                title="Speciality"
+                                id="speciality-dropdown"
+                                show={showDropdown === 'speciality'}
+                                onMouseEnter={() => handleMouseEnter('speciality')}
+                                onMouseLeave={handleMouseLeave}
+                            >
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something else here</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title="Patient Info" id="basic-nav-dropdown">
+
+                          
+                            <NavDropdown
+                                title="Patient Info"
+                                id="patient-info-dropdown"
+                                show={showDropdown === 'patient-info'}
+                                onMouseEnter={() => handleMouseEnter('patient-info')}
+                                onMouseLeave={handleMouseLeave}
+                            >
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something else here</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title="Hospitals" id="basic-nav-dropdown">
+
+                          
+                            <NavDropdown
+                                title="Hospitals"
+                                id="hospitals-dropdown"
+                                show={showDropdown === 'hospitals'}
+                                onMouseEnter={() => handleMouseEnter('hospitals')}
+                                onMouseLeave={handleMouseLeave}
+                            >
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something else here</NavDropdown.Item>
