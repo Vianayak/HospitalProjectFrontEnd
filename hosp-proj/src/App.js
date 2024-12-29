0c93b2fa133e80436017ebd8abee4f3a62f5367e
gigsowjanya <sowjanya.nallanagula@gigleaz.com>
2024-12-29 19:53:27 +0530

    carousel update by sowjanya

--- hosp-proj/src/App.js
+++ hosp-proj/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
+import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HeroSection/>
     </div>
   );
 }
---
+++ hosp-proj/src/components/HeroSection/HeroSection.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './HeroSection.css'; // Import your CSS file
+
+const HeroSection = () => {
+  return (
+    <div id="carouselExampleControls" className="carousel slide custom-carousel" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img className="d-block w-100" src="/Assets/Images/CarouselImage1.jpg" alt="First slide" />
+        </div>
+        <div className="carousel-item">
+          <img className="d-block w-100" src="/Assets/Images/CarouselImage2.jpg" alt="Second slide" />
+        </div>
+        
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export default HeroSection;
