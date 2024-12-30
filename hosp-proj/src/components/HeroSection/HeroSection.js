fa6693faac55a92617742d942c0bcf1451f78f08
gigsowjanya <sowjanya.nallanagula@gigleaz.com>
2024-12-30 19:21:48 +0530

    herosec update

--- hosp-proj/src/components/HeroSection/HeroSection.js
+++ hosp-proj/src/components/HeroSection/HeroSection.js
@@ -6,17 +6,12 @@ const HeroSection = () => {
     <div id="carouselExampleControls" className="carousel slide custom-carousel" data-bs-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img className="d-block w-100" src="/Assets/Images/CarouselImage1.jpg" alt="First slide" />
+          <img className="d-block w-100" src="/Assets/Images/Image1.jpg" alt="First slide" />
         </div>
         <div className="carousel-item">
-          <img className="d-block w-100" src="/Assets/Images/CarouselImage2.jpg" alt="Second slide" />
+          <img className="d-block w-100" src="/Assets/Images/Image2.jpg" alt="Second slide" />
         </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src="/Assets/Images/CarouselImage3.jpg" alt="Third slide" />
-        </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src="/Assets/Images/CarouselImage4.jpg" alt="Fourth slide" />
-        </div>
+       
       </div>
       <button
         className="carousel-control-prev"
