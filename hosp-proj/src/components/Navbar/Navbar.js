6d7b28c2be27599dbc4d800ad758282e24a8c01d
gigsowjanya <sowjanya.nallanagula@gigleaz.com>
2024-12-30 19:32:15 +0530

    fixed navbar

--- hosp-proj/src/components/Navbar/Navbar.js
+++ hosp-proj/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MyNavbar.css';
 
