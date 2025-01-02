a844e3bce41fbdb909eec2324f106ec9f0bed469
gigsowjanya <sowjanya.nallanagula@gigleaz.com>
2025-01-02 18:09:49 +0530

    logo redirection to main page

--- hosp-proj/src/components/Navbar/Navbar.js
+++ hosp-proj/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MyNavbar.css';
 import SearchBar from '../SearchBar/SearchBar';
@@ -18,82 +19,81 @@ function MyNavbar() {
     return (
         <header>
             <Navbar bg="white" variant="light" expand="lg" className="sticky-navbar">
-             
-                    <Navbar.Brand href="#">
+                <Navbar.Brand>
+                    {/* Use Link to redirect to the main page */}
+                    <Link to="/" className="navbar-logo-link">
                         <img
                             src="/Assets/Images/NewLogo.jpeg"
                             alt="Jaya Hospitals Logo"
                             className="navbar-logo"
                         />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto" id="exe">
-                            <Nav.Link href="#home">About Us</Nav.Link>
-                            <Nav.Link href="#features">Our Doctors</Nav.Link>
-                            <NavDropdown
-                                title={
-                                    <span
-                                        className={`dropdown-title ${
-                                            showDropdown === 'speciality' ? 'active' : ''
-                                        }`}
-                                    >
-                                        Speciality
-                                    </span>
-                                }
-                                id="speciality-dropdown"
-                                show={showDropdown === 'speciality'}
-                                onMouseEnter={() => handleMouseEnter('speciality')}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                <NavDropdown.Item href="#action/3.1">General Medicine</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">ENT</NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown
-                                title={
-                                    <span
-                                        className={`dropdown-title ${
-                                            showDropdown === 'patient-info' ? 'active' : ''
-                                        }`}
-                                    >
-                                        Patient Info
-                                    </span>
-                                }
-                                id="patient-info-dropdown"
-                                show={showDropdown === 'patient-info'}
-                                onMouseEnter={() => handleMouseEnter('patient-info')}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                <NavDropdown.Item href="#action/3.1">Blog</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">News</NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown
-                                title={
-                                    <span
-                                        className={`dropdown-title ${
-                                            showDropdown === 'hospitals' ? 'active' : ''
-                                        }`}
-                                    >
-                                        Hospitals
-                                    </span>
-                                }
-                                id="hospitals-dropdown"
-                                show={showDropdown === 'hospitals'}
-                                onMouseEnter={() => handleMouseEnter('hospitals')}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                <NavDropdown.Item href="#action/3.1">Jubilee Hills</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Kukatpally</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Miyapur</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-               
-
-              
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto" id="exe">
+                        <Nav.Link as={Link} to="/">About Us</Nav.Link>
+                        <Nav.Link as={Link} to="/features">Our Doctors</Nav.Link>
+                        <NavDropdown
+                            title={
+                                <span
+                                    className={`dropdown-title ${
+                                        showDropdown === 'speciality' ? 'active' : ''
+                                    }`}
+                                >
+                                    Speciality
+                                </span>
+                            }
+                            id="speciality-dropdown"
+                            show={showDropdown === 'speciality'}
+                            onMouseEnter={() => handleMouseEnter('speciality')}
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <NavDropdown.Item as={Link} to="/general-medicine">General Medicine</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/ent">ENT</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown
+                            title={
+                                <span
+                                    className={`dropdown-title ${
+                                        showDropdown === 'patient-info' ? 'active' : ''
+                                    }`}
+                                >
+                                    Patient Info
+                                </span>
+                            }
+                            id="patient-info-dropdown"
+                            show={showDropdown === 'patient-info'}
+                            onMouseEnter={() => handleMouseEnter('patient-info')}
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <NavDropdown.Item as={Link} to="/blog">Blog</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/news">News</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown
+                            title={
+                                <span
+                                    className={`dropdown-title ${
+                                        showDropdown === 'hospitals' ? 'active' : ''
+                                    }`}
+                                >
+                                    Hospitals
+                                </span>
+                            }
+                            id="hospitals-dropdown"
+                            show={showDropdown === 'hospitals'}
+                            onMouseEnter={() => handleMouseEnter('hospitals')}
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <NavDropdown.Item as={Link} to="/jubilee-hills">Jubilee Hills</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/kukatpally">Kukatpally</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/miyapur">Miyapur</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
                 <div className="search-bar-container">
-                   <SearchBar />
-              </div>
+                    <SearchBar />
+                </div>
             </Navbar>
         </header>
     );
