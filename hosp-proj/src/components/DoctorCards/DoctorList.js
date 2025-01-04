1294686db55c71e6411a9fd1d833f15aaae9fd6d
vinayak banoth <code.editor>
2025-01-04 10:28:26 +0530

    Updated doctorlist where getting list dynamically

--- hosp-proj/src/components/DoctorCards/DoctorList.js
+++ hosp-proj/src/components/DoctorCards/DoctorList.js
@@ -1,47 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DoctorCard from "./DoctorCards";
 import "./DoctorCards.css";
-
-const doctors = [
-  {
-    name: "Dr. A. Kumar",
-    speciality: "Cardiologist",
-    experience: 15,
-    location: "Apollo Hospitals, Chennai",
-    availability:"MON-SAT(10AM-5PM)",
-    image: "Assets/Images/Love.jpeg", // Replace with actual image URL
-  },
-  {
-    name: "Dr. B. Sharma",
-    speciality: "Orthopedic Surgeon",
-    experience: 10,
-    location: "Apollo Hospitals, Delhi",
-    availability:"MON-SAT(10AM-5PM)",
-    image: "Assets/Images/Love.jpeg", // Replace with actual image URL
-  },
-  {
-    name: "Dr. C. Rao",
-    speciality: "Dermatologist",
-    experience: 8,
-    location: "Apollo Hospitals, Bangalore",
-    availability:"MON-SAT(10AM-5PM)",
-    image: "Assets/Images/Love.jpeg", // Replace with actual image URL
-  },
-  {
-    name: "Dr. D. Singh",
-    speciality: "Pediatrician",
-    experience: 12,
-    location: "Apollo Hospitals, Hyderabad",
-    availability:"MON-SAT(10AM-5PM)",
-    image: "Assets/Images/Love.jpeg", // Replace with actual image URL
-  },
-];
+import axios from "axios";
 
 const DoctorList = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    // Fetching data from the backend
+    axios
+      .get("http://localhost:8081/api/doctors/doctors-list")
+      .then((response) => {
+        setDoctors(response.data); // Update state with fetched data
+      })
+      .catch((error) => {
+        console.error("Error fetching doctors data:", error);
+      });
+  }, []);
+
   return (
     <div className="doctor-list">
-      {doctors.map((doctor, index) => (
-        <DoctorCard key={index} doctor={doctor} />
+      {doctors.map((doctor) => (
+        <DoctorCard key={doctor.id} doctor={doctor} />
       ))}
     </div>
   );
