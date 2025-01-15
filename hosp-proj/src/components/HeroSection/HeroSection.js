e1d14ba34a9cf0a19f4ac29b82db9df30f6574dd
gigsowjanya <sowjanya.nallanagula@gigleaz.com>
2025-01-15 12:03:28 +0530

    image

--- hosp-proj/src/components/HeroSection/HeroSection.js
+++ hosp-proj/src/components/HeroSection/HeroSection.js
@@ -5,16 +5,16 @@ const HeroSection = () => {
   return (
     <div id="carouselExampleControls" className="carousel slide custom-carousel" data-bs-ride="carousel">
       <div className="carousel-inner">
-        
-        <div className="carousel-item">
-          <img className="d-block w-100" src="/Assets/Images/Image2.jpg" alt="first slide" />
+       
+        <div className="carousel-item active">
+          <img className="d-block w-100" src="/Assets/Images/Image2.jpg" alt="Second slide" />
         </div>
         
         <div className="carousel-item">
           <img className="d-block w-100" src="/Assets/Images/coro1.png" alt="Second slide" />
         </div>
         <div className="carousel-item">
-          <img className="d-block w-100" src="/Assets/Images/Image5.jpg" alt="Third slide" />
+          <img className="d-block w-100" src="/Assets/Images/Image5.jpg" alt="Second slide" />
         </div>
       </div>
       <button
